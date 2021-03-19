3abd308df46705c1148c4e302e743e6856ab4df5
Yik San Chan <evan.chanyiksan@gmail.com>
2021-03-19 12:19:08 +0800

    Use SQL

--- udf_sum.py
+++ udf_sum.py
@@ -1,42 +1,45 @@
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.table import StreamTableEnvironment, DataTypes
-from pyflink.table.descriptors import Schema, OldCsv, FileSystem
 from pyflink.table.udf import udf
 
 # https://flink.apache.org/2020/04/09/pyflink-udf-support-flink.html
 # https://ci.apache.org/projects/flink/flink-docs-release-1.12/dev/python/table-api-users-guide/udfs/python_udfs.html
 
-@udf(input_types=[DataTypes.BIGINT(), DataTypes.BIGINT()], result_type=DataTypes.BIGINT())
+@udf(input_types=[DataTypes.INT(), DataTypes.INT()], result_type=DataTypes.BIGINT())
 def add(i, j):
   return i + j
 
 env = StreamExecutionEnvironment.get_execution_environment()
-env.set_parallelism(1)
 t_env = StreamTableEnvironment.create(env)
 
 t_env.register_function("add", add)
 
-t_env.connect(FileSystem().path('/tmp/input')) \
-    .with_format(OldCsv()
-                 .field('a', DataTypes.BIGINT())
-                 .field('b', DataTypes.BIGINT())) \
-    .with_schema(Schema()
-                 .field('a', DataTypes.BIGINT())
-                 .field('b', DataTypes.BIGINT())) \
-    .create_temporary_table('mySource')
+my_source_ddl = """
+create table mySource (
+    a INT,
+    b INT
+) with (
+    'connector' = 'datagen',
+    'rows-per-second' = '5'
+)
+"""
 
-t_env.connect(FileSystem().path('/tmp/output')) \
-    .with_format(OldCsv()
-                 .field('sum', DataTypes.BIGINT())) \
-    .with_schema(Schema()
-                 .field('sum', DataTypes.BIGINT())) \
-    .create_temporary_table('mySink')
+my_sink_ddl = """
+create table mySink (
+    c BIGINT
+) with (
+    'connector' = 'print'
+)
+"""
 
-t_env.from_path('mySource')\
-    .select("add(a, b)") \
-    .insert_into('mySink')
+my_transform_dml = """
+insert into mySink
+select add(a, b)
+from mySource
+"""
 
-t_env.execute("Sum")
-
-# before run: echo "1,2" > /tmp/input
-# after run: cat /tmp/output
+t_env.execute_sql(my_source_ddl)
+t_env.execute_sql(my_sink_ddl)
+t_env.execute_sql(my_transform_dml).wait()
+# t_env.from_path("mySource").select("add(a,b)").insert_into("mySink")
+# t_env.execute("Sum")
