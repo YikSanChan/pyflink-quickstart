cd003ca7d36583999dbb5ffd45958762e4323607
Yik San Chan <evan.chanyiksan@gmail.com>
2021-04-27 20:50:43 +0800

    myth

--- decrypt_fun.py
+++ decrypt_fun.py
@@ -1,12 +1,8 @@
 from pyflink.table.udf import udf, ScalarFunction
 from pyflink.table import DataTypes
 
-class Decrypt(ScalarFunction):
-    def open(self, function_context):
-        import pandas as pd
-        self.d = pd.read_csv('resources.zip/resources/crypt.csv', header=None, index_col=0, squeeze=True).to_dict()
-
-    def eval(self, s):
-        return self.d.get(s, "unknown")
-
-decrypt = udf(Decrypt(), input_types=[DataTypes.STRING()], result_type=DataTypes.STRING())
+@udf(input_types=[DataTypes.STRING()], result_type=DataTypes.STRING())
+def decrypt(s):
+    import pandas as pd
+    d = pd.read_csv('resources.zip/resources/crypt.csv', header=None, index_col=0, squeeze=True).to_dict()
+    return d.get(s, "unknown")
