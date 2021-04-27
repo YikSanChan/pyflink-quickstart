9d5e59adbd5598afc20db1c812f672a755928e8e
Yik San Chan <evan.chanyiksan@gmail.com>
2021-04-27 19:31:08 +0800

    override udf open()

--- udf_use_resource.py
+++ udf_use_resource.py
@@ -1,12 +1,16 @@
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.table import StreamTableEnvironment, DataTypes
-from pyflink.table.udf import udf
+from pyflink.table.udf import udf, ScalarFunction
 
-@udf(input_types=[DataTypes.STRING()], result_type=DataTypes.STRING())
-def decrypt(s):
-    import pandas as pd
-    d = pd.read_csv('resources.zip/resources/crypt.csv', header=None, index_col=0, squeeze=True).to_dict()
-    return d.get(s, "unknown")
+class Decrypt(ScalarFunction):
+    def open(self, function_context):
+        import pandas as pd
+        self.d = pd.read_csv('resources.zip/resources/crypt.csv', header=None, index_col=0, squeeze=True).to_dict()
+
+    def eval(self, s):
+        return self.d.get(s, "unknown")
+
+decrypt = udf(Decrypt(), input_types=[DataTypes.STRING()], result_type=DataTypes.STRING())
 
 env = StreamExecutionEnvironment.get_execution_environment()
 t_env = StreamTableEnvironment.create(env)
