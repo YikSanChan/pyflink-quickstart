144f300dc1c6272512c9bdd838bddcff75b1bc7b
Yik San Chan <evan.chanyiksan@gmail.com>
2021-04-27 17:16:40 +0800

    fix: use resource in udf

--- udf_use_resource.py
+++ udf_use_resource.py
@@ -5,7 +5,7 @@ from pyflink.table.udf import udf
 @udf(input_types=[DataTypes.STRING()], result_type=DataTypes.STRING())
 def decrypt(s):
     import pandas as pd
-    d = pd.read_csv('resources/crypt.csv', header=None, index_col=0, squeeze=True).to_dict()
+    d = pd.read_csv('resources.zip/resources/crypt.csv', header=None, index_col=0, squeeze=True).to_dict()
     return d.get(s, "unknown")
 
 env = StreamExecutionEnvironment.get_execution_environment()
